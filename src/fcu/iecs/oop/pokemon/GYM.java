4b42f898188c79e4e2933d245bc5b81811397fa9
user <user@asus>
2016-11-25 18:42:39 +0800

    功課五

--- src/fcu/iecs/oop/pokemon/GYM.java
+++ src/fcu/iecs/oop/pokemon/GYM.java
@@ -7,13 +7,12 @@ public class GYM {
 		Random a=new Random();
 		int b;
 		b= a.nextInt(2);
-
 		if(b==0){
 			p1.setCp(p1.getCp()+500);
-			System.out.println(p1.getName());
+			System.out.println("Winner is \""+p1.getName()+"\"");
 		}else if(b==1){
 			p2.setCp(p2.getCp()+500);
-			System.out.println(p2.getName());
+			System.out.println("Winner is \""+p2.getName()+"\"");
 		}
 	}
 
