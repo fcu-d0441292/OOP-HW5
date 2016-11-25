afdf9a8e5302e5170c1c2525009b6fa9366f3e1c
user <user@asus>
2016-11-25 18:31:24 +0800

    作業五

--- src/fcu/iecs/oop/pokemon/GYM.java
+++ src/fcu/iecs/oop/pokemon/GYM.java
@@ -6,12 +6,12 @@ public class GYM {
 	public static void fight(Pokemon p1, Pokemon p2){
 		Random a=new Random();
 		int b;
-		b= a.nextInt(2)+1;
-		System.out.println(b);
-		if(b==1){
+		b= a.nextInt(2);
+
+		if(b==0){
 			p1.setCp(p1.getCp()+500);
 			System.out.println(p1.getName());
-		}else if(b==2){
+		}else if(b==1){
 			p2.setCp(p2.getCp()+500);
 			System.out.println(p2.getName());
 		}
